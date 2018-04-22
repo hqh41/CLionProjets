3678cc812c9b0b091f94c44273f5d3d37a3a3819
hqh41 <hqh41@outlook.com>
2018-04-22 15:16:08 +0200

    4.22

---
+++ N_Queens/cmake-build-debug/CMakeFiles/N_Queens.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/N_Queens.dir/main.cpp.o"
+  "N_Queens.pdb"
+  "N_Queens"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/N_Queens.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
